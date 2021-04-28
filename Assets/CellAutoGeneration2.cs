a1a93440a9c2d36d3f3374500b6bc26001d7228c
sericaer <sericaer@outlook.com>
2021-04-28 23:19:40 +0800

    修改方法

--- Assets/CellAutoGeneration2.cs
+++ Assets/CellAutoGeneration2.cs
@@ -27,11 +27,11 @@ namespace MapGenerator
 
             activeCell[(size / 2, size / 2)] = 0;
 
-            while(stableCell.Count() < size*size)
+            while(activeCell.Count() != 0)
             {
                 int random = UnityEngine.Random.Range(0, size * size);
 
-                var currKey = activeCell.ElementAt(random% activeCell.Count()).Key;
+                var currKey = activeCell.Keys.ElementAt(random% activeCell.Count());
 
                 var emptyNearbys = GetEmptyNearbys(currKey);
                 if (emptyNearbys.Count() == 0)
@@ -41,28 +41,45 @@ namespace MapGenerator
                     continue;
                 }
 
-                var nearby = emptyNearbys.ElementAt(random%emptyNearbys.Count());
+                var nearby = emptyNearbys.ElementAt(random % emptyNearbys.Count());
                 var cellNearbys = GetCellNearbys(nearby);
-                if(random%100<10 || cellNearbys.Values.Where(x => x).Count() * 100 / cellNearbys.Count() > 50)
+
+                var sum = cellNearbys.Values.Sum();
+                if (sum != 0 && UnityEngine.Random.Range(0, 100) < 60)
                 {
                     activeCell[nearby] = 1;
                 }
                 else
                 {
-                    activeCell[nearby] = 0;
+                    if (random % 100 < 10)
+                    {
+                        activeCell[nearby] = 1;
+                    }
+                    else
+                    {
+                        activeCell[nearby] = 0;
+                    }
+                    
                 }
-                
             }
 
+            var rslt = stableCell.ToDictionary(key=>key.Key, value=>0);
+            var keys = stableCell.Keys.Where(x => stableCell[x] != 0);
+            foreach(var key in keys)
+            {
+                var nearbys = GetNearbys(key);
 
-            return stableCell;
+                var count = nearbys.Where(x => stableCell[x] != 0).Count();
+                rslt[key] = Enumerable.Range(0, count*3).Sum(_ => UnityEngine.Random.Range(count/2, count));
+            }
+
+            return rslt;
         }
 
         private IEnumerable<(int x, int y)> GetEmptyNearbys((int x, int y) key)
         {
             return GetNearbys(key).Where(e => !activeCell.ContainsKey(e) && !stableCell.ContainsKey(e)).ToArray();
         }
-
 
         private IEnumerable<(int x, int y)> GetNearbys((int x, int y) key)
         {
@@ -72,9 +89,11 @@ namespace MapGenerator
             return rslt.Where(e => e.x < size && e.x >= 0 && e.y < size && e.y >= 0);
         }
 
-        private Dictionary<(int x, int y), bool> GetCellNearbys((int x, int y) key)
+        private Dictionary<(int x, int y), int> GetCellNearbys((int x, int y) key)
         {
-            return GetNearbys(key).ToDictionary(key => key, value => stableCell.ContainsKey(key) || activeCell.ContainsKey(key));
+            var nearbys = GetNearbys(key);
+
+            return nearbys.Where(x=> stableCell.ContainsKey(x) || activeCell.ContainsKey(x)).ToDictionary(key => key, value => stableCell.ContainsKey(value)? stableCell[value] : activeCell[value]);
         }
 
         private int[] GetRandomArray(int min, int max, int count)
